d0ade3598b6c399ed587a9342f9509cafffb8918
ewrefads <gdonovan@live.dk>
2024-04-30 14:04:27 +0200

    Added wizard player character and skeleton code for abilities

---
+++ Source/ThirdRowShooter/Ability.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Ability.generated.h"
+
+UCLASS()
+class THIRDROWSHOOTER_API AAbility : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AAbility();
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float castTime;
+
+	UPROPERTY(BlueprintReadOnly)
+	float remainingCastTime = 0;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float cooldown;
+
+	UPROPERTY(BlueprintReadOnly)
+	float remainingCooldown = 0;
+
+	UFUNCTION(BlueprintCallable)
+	void BeginCast();
+
+	UFUNCTION(BlueprintCallable)
+	void CancelCast();
+
+	UFUNCTION(BlueprintCallable)
+	void CancelCooldown();
+
+	UFUNCTION(BlueprintCallable)
+	virtual void Cast();
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
